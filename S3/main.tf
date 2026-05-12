306bee1f5e31cdaef8cc552b432d9a41631bdf75
heysagar-max <prajapatsagar725@gmail.com>
2026-05-12 12:40:16 +0530

    Add files via upload

--- S3/main.tf
+++ S3/main.tf
@@ -1,1 +1,18 @@
+resource "aws_s3_bucket" "mybucket" {
+  bucket = "bucckeeet08022026"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_object" "object" {
+  bucket = aws_s3_bucket.mybucket.bucket
+  key    = "text.txt"
+  source = "${path.module}/text.txt"
+
+  etag       = filemd5("${path.module}/text.txt")
+  depends_on = [aws_s3_bucket.mybucket]
+}
 
---
+++ S3/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.28.0"
+    }
+  }
+}
+
+provider "aws" {
+  access_key = "AKIA2MXU2UXQRA4FM4EV"
+  secret_key = "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"
+  region     = "eu-north-1"
+}
