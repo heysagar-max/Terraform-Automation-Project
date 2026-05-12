afa6ce58add13d5d72883df31f76040f1e5a5c3c
heysagar-max <prajapatsagar725@gmail.com>
2026-05-12 12:28:15 +0530

    Update main.tf

--- variable/main.tf
+++ variable/main.tf
@@ -1,1 +1,24 @@
+variable "username" {
+  type        = string
+  description = "provide the username"
+  default     = "nik"
+
+}
+
+variable "age" {
+  type        = number
+  description = "provide the age"
+  default     = "22"
+
+}
+
+
+
+output "user_name" {
+  value = "my name is ${var.username}"
+}
+
+output "user_age" {
+  value = "mention the provide age is ${var.age}"
+}
 
